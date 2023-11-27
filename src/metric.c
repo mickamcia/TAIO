1e5a01103a5309e241a0be4f03a2a7448b6c2f0a
lubaszkad <01161398@pw.edu.pl>
2023-11-27 20:18:27 +0100

    distance func

--- src/metric.c
+++ src/metric.c
@@ -8,21 +8,95 @@
 // internal
 int* build_degree_vector(matrix* g, int* newSize);
 int vector_diff(int* v1, int* v2, int size);
+int get_graph_n(matrix* g);
+int get_graph_m_connected(matrix* g);
+int sum_acc(int g1_n, int g2_n);
+float err(matrix* g1, matrix* g2, int n, int m);
 
 int distance(matrix* g1, matrix* g2) {
-	int v1_size, v2_size;
-	int* v1 = build_degree_vector(g1, &v1_size);
-	int* v2 = build_degree_vector(g2, &v2_size);
 
-	if (v1_size != v2_size)
-		return INT_MAX;
+	int g1_n, g1_m, g2_n, g2_m;
+	int sum_acc_value;
+	float err_value = 0; 
 
-	int d = vector_diff(v1, v2, v1_size);
+	g1_n = get_graph_n(g1);
+	g2_n = get_graph_n(g2);
+	g1_m = get_graph_m_connected(g1);
+	g2_m = get_graph_m_connected(g2);
+	
+	//printf("\ng1_n: %d, g2_n: %d, g1_m: %d, g2_m: %d\n", g1_n, g2_n, g1_m, g2_m);
+	
+	if (g1_n >= g2_n) {
+	
+		sum_acc_value = sum_acc(g2_n, g1_n);
+	}
+	else 
+	{
+		sum_acc_value = sum_acc(g1_n, g2_n);
+	}
+	
+	if (g1_n == g2_n && g1_m == g2_m)
+	{
+		err_value = err(g1, g2, g1_n,g1_m);
+	}
 
-	free(v1);
-	free(v2);
+	return  sum_acc_value + abs(g1_n - g2_n) + abs(g1_m - g2_m) + err_value;
+}
 
-	return d;
+
+int get_graph_n(matrix* g) 
+{
+	int size = 0;
+	for (int i = 0; i < g->size; i++)
+		if (g->mat[i * g->size + i] == 0)
+			size++;
+	return size;
+}
+
+int get_graph_m_connected(matrix* g) 
+{
+	int m = 0;
+	for (int i = 0; i < g->size; i++) {
+		if (g->mat[i * g->size + i] == 0) {
+			for (int j = 0; j < g->size; j++) {
+				if (g->mat[i * g->size + j] > 0) {
+					m++;
+				}
+			}
+		}
+	}
+	return m;
+}
+
+int sum_acc(int g1_n, int g2_n)
+{
+	int result = 0;
+	for (int i = g1_n; i < g2_n; i++)
+		result += (i * (i - 1));
+	return result;
+}
+
+float err(matrix* g1, matrix* g2, int n, int m) {
+	
+	float err_outgoing_edges_acc_diff;
+	float err_outgoing_edges_diff;
+	float err_neighbour_outgoing_edges_acc_diff;
+	float err_neighbour_outgoing_edges_diff;
+	
+
+
+	//int* v1 = build_degree_vector(g1, &v1_size);
+	//int* v2 = build_degree_vector(g2, &v2_size);
+
+	//if (v1_size != v2_size)
+	//	return INT_MAX;
+
+	//int d = vector_diff(v1, v2, v1_size);
+
+	//free(v1);
+	//free(v2);
+
+	return 0;
 }
 
 int* build_degree_vector(matrix* g, int* newSize) {
--- src/metric.h
+++ src/metric.h
@@ -4,6 +4,6 @@
 #include <stdio.h>
 #include "matrix.h"
 
-int distance(matrix* g1, matrix* g2);
+float distance(matrix* g1, matrix* g2);
 
 #endif
