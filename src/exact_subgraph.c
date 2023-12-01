4d3a9c13bd71b855fe2ba111b0113456b53f1029
mickamcia desktop <mkciasnocha@gmail.com>
2023-12-01 17:55:47 +0100

    does it work?

--- src/exact_subgraph.c
+++ src/exact_subgraph.c
@@ -7,12 +7,17 @@
 // driver function
 void exact_subgraph_run(matrix* a, matrix* b)
 {
-    matrix* mod_prod = modular_product(a, b);
-    matrix* clique = matrix_clone(mod_prod);
+
+	matrix* mod_prod = modular_product(a, b);
+	matrix* sym_mod_prod = matrix_clone(mod_prod);
+    symmetrize_max(sym_mod_prod);
+
+    matrix* clique = matrix_clone(sym_mod_prod);
 
     exact_clique_bb_run(clique);
-    extract_solution(clique, a, b);
+    extract_solution(mod_prod, clique, a, b);
     
     matrix_destroy(mod_prod);
     matrix_destroy(clique);
+    matrix_destroy(sym_mod_prod);
 }
--- src/subgraph.h
+++ src/subgraph.h
@@ -3,7 +3,9 @@
 
 #include "matrix.h"
 
+void symmetrize_max(matrix* m);
+matrix* edge_coding(matrix* a, matrix* b);
 matrix* modular_product(matrix* a, matrix* b);
-void extract_solution(matrix* clique, matrix* a, matrix* b);
+void extract_solution(matrix* mod_prod, matrix* clique, matrix* a, matrix* b);
 
 #endif // !SUBGRAPH_H
