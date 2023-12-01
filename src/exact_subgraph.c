bb3dbb3fc8f1df2db3bc8a6c06f43a240bb04d9b
mickamcia desktop <mkciasnocha@gmail.com>
2023-12-01 21:26:56 +0100

    comment debug stuff

--- src/exact_subgraph.c
+++ src/exact_subgraph.c
@@ -16,9 +16,9 @@ void exact_subgraph_run(matrix* a, matrix* b)
 
     exact_clique_bb_run(clique);
     extract_solution(mod_prod, clique, a, b);
-    graph_print(mod_prod, "mod_prod");
-    graph_print(sym_mod_prod, "sym_mod_prod");
-    graph_print(clique, "clique");
+    //graph_print(mod_prod, "mod_prod");
+    //graph_print(sym_mod_prod, "sym_mod_prod");
+    //graph_print(clique, "clique");
     
     matrix_destroy(mod_prod);
     matrix_destroy(clique);
